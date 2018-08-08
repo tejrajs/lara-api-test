6eb84b28481608e4bc26d3f7bcb695083e37aa0a
Tej Raj Shrestha <helloteju96@gmail.com>
2018-08-09 00:44:18 +1000

    model property added with between scope

--- app/Property.php
+++ app/Property.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Property extends Model
 {
-    //
+    //Table Name
+	protected $table = "properties";
+	 
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array
+	 */
+	protected $fillable = [
+	
+			'name', 'price', 'bedrooms', 'bathrooms', 'storeys', 'garages'
+	];
+	
+	public function scopeBetween($query, $fprice=0, $sprice=0)
+	{
+		return $query->whereBetween('price', [$fprice, $sprice]);
+	}
 }
